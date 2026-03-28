209e6790cbea046cea0ae7506fd6606e8b173416
Nathalie Leal <naathalieleal@gmail.com>
2026-03-27 21:34:21 -0300

    feat: adiciona contagem de parágrafos com e sem palavras duplicadas e melhora formatação da saída

--- src/helpers.js
+++ src/helpers.js
@@ -5,17 +5,25 @@ function filtraOcorrencias(paragrafo) {
 function montaSaidaArquivo(listaPalavras) {
     let textoFinal = '';
     let paragrafosSemDuplicadas = [];
-   
+ 
+    let comDuplicadas  = 0;
+    let semDuplicadas  = 0;
+    
     listaPalavras.forEach((paragrafo, indice) => {
-        const duplicadas = filtraOcorrencias(paragrafo).join(', ');
-
+        const duplicadas  = filtraOcorrencias(paragrafo);
+        
         if(duplicadas.length > 0 ) {
-            textoFinal += `palavras duplicadas no parágrafo ${indice + 1}: ${duplicadas} \n`
+            textoFinal += `palavras duplicadas no parágrafo ${indice + 1}: ${duplicadas.join(', ')} \n`;
+            comDuplicadas++;
         } else {
             paragrafosSemDuplicadas.push(indice + 1);
-        }
+            textoFinal += `sem palavras duplicadas no parágrafo ${indice + 1} \n`;
+            semDuplicadas++ ;
+        }       
     })
-    return textoFinal;
+
+    return `Quantidade de parágrafos com palavras duplicadas: ${comDuplicadas}\nQuantidade de parágrafos sem palavras duplicadas: ${semDuplicadas}\n\n${textoFinal}`        
+    
 }
 
 export { montaSaidaArquivo };
